7ebf6526d69377082de3332c174ffd97b47d1b14
Pedro Scarapicchia Junior <pscarapicchiajun@paypal.com>
2019-08-13 16:09:40 -0300

    Get Approval URL from API

--- app/src/main/java/com/example/cctexample/HandleReturn.kt
+++ app/src/main/java/com/example/cctexample/HandleReturn.kt
@@ -4,6 +4,8 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import android.content.Intent
 
 class HandleReturn : AppCompatActivity() {
 
@@ -17,6 +19,12 @@ class HandleReturn : AppCompatActivity() {
         val baToken = data?.getQueryParameter("ba_token")
 
         val msg = if (baToken != null) "User accepted Billing Agreement" else "User canceled"
+
+        val myIntent = Intent(this, MainActivity::class.java)
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+
+        startActivity(myIntent)
 
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
--- app/src/main/java/com/example/cctexample/MainActivity.kt
+++ app/src/main/java/com/example/cctexample/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.cctexample
 
-import android.content.Context
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
-
-
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,9 +27,32 @@ class MainActivity : AppCompatActivity() {
 
             val intentBuilder = CustomTabsIntent.Builder()
             val customTabsIntent = intentBuilder.build()
-            customTabsIntent.launchUrl(v.context, uri)
+
+            customTabsIntent.launchUrl(this, uri)
         } else {
-            Toast.makeText(this, "Please input an approval URL", Toast.LENGTH_LONG).show()
+            Toast.makeText(v.context, "Please input an approval URL", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun getApproval(v: View) {
+        val httpAsync = "https://runkit.io/pedrinho/approval-url/8.0.0"
+            .httpGet()
+            .responseString { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex)
+                        Toast.makeText(v.context, "Error getting Approval URL", Toast.LENGTH_LONG).show()
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        val text = findViewById<View>(R.id.approval_url) as EditText
+                        text.setText(data, TextView.BufferType.EDITABLE)
+                        println(data)
+                    }
+                }
+            }
+
+        httpAsync.join()
     }
 }
